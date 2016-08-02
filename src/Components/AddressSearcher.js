21f35b01adf104f66d79aadf2ea0962bb75e3db7
Logan Elliott <logansails@gmail.com>
2016-08-03 00:12:47 +1200

    Clean up function for find board

--- src/Components/AddressSearcher.js
+++ src/Components/AddressSearcher.js
@@ -2,52 +2,75 @@ import React, {PropTypes} from 'react'
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import axios from 'axios';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+const koordinatesLayerId = 1513;
+const koordinatesApiKey = '979e84540aac481685f1e9ea5331cc35';
+const googleApiKey = 'AIzaSyAE7vD-Xl1RjQ_PPzinv2omvZy1HqiHI3c';
 
 export default class AddressSearcher extends React.Component {
     constructor(props, context) {
         super(props, context);
 
         this.state = {
-            ward: "",
+            localBoard: "",
+            value: ""
         };
 
         this.handleClick = this.handleClick.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
+    handleChange(event) {
+        this.setState({
+            value: event.target.value,
+        });
+    };
+
     handleClick() {
-        axios.get('https://maps.googleapis.com/maps/api/geocode/json?address=29+leighton+st,+grey+lynn,+Auckland&key=AIzaSyAE7vD-Xl1RjQ_PPzinv2omvZy1HqiHI3c')
+        this.getLatLngFromAddress(this.state.value);
+    }
+
+    getLatLngFromAddress(address){
+        axios.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + googleApiKey)
             .then((response) => {
-                console.debug(response.data);
                 let lat = response.data.results[0].geometry.location.lat;
                 let lng = response.data.results[0].geometry.location.lng;
-                axios.get("http://api.koordinates.com/api/vectorQuery.json/?key=979e84540aac481685f1e9ea5331cc35&layer=1349&x=" + lng + "&y=" + lat)
-                    .then((koordinatesResponse) => {
-                        console.debug(koordinatesResponse.data);
 
-                        this.setState({ward: koordinatesResponse.data.vectorQuery.layers[1349].features[0].properties.WARD_NAME});
-                    })
-                    .catch((err) => {
-                        console.debug(err)
-                    });
+                this.getLocalBoardFromLatLng(lat,lng);
             })
             .catch((err) => {
                 console.debug(err)
             });
-
-
     }
 
-
+    getLocalBoardFromLatLng(lat, lng){
+        axios.get('http://api.koordinates.com/api/vectorQuery.json/?key=' + koordinatesApiKey + '&layer=' + koordinatesLayerId + '&x=' + lng + '&y=' + lat)
+            .then((koordinatesResponse) => {
+                let localBoard = koordinatesResponse.data.vectorQuery.layers[koordinatesLayerId].features[0].properties.CB_NAME;
+                if(localBoard === 'Te Irirangi Local Board Area'){
+                    localBoard = 'Howick Local Board Area';
+                }
+                this.setState({localBoard: localBoard});
+            })
+            .catch((err) => {
+                console.debug(err)
+            });
+    }
 
     render() {
 
         return(
             <div>
-                <TextField hintText="Enter Address"/>
-                <RaisedButton label="Find Ward"
+                <TextField hintText="Enter Address"
+                           value={this.state.value}
+                           onChange={this.handleChange}
+                />
+                <RaisedButton label="Find Local Board"
                               onClick={this.handleClick}
                 />
-                {this.state.ward}
+                <div>Your Local Board is: {this.state.localBoard}</div>
             </div>
         );
     }
