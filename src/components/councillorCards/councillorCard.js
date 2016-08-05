e6f71f3178f5e07270f45e0316f6eb93bfefae84
Logan Elliott <logansails@gmail.com>
2016-08-06 00:03:40 +1200

    Genericise councillor card & questions

--- src/components/councillorCards/councillorCard.js
+++ src/components/councillorCards/councillorCard.js
@@ -13,26 +13,62 @@ export default class councillorCard extends React.Component {
         super(props, context);
 
         this.state = {
-            email: 'info@generationzero.org.nz',
-            website: 'http://www.generationzero.org/',
-            name: 'Generation Zero',
-            board: 'Waitamata',
-            avatar: 'gzLogo.jpg',
-            grade: 'A+',
-            expanded: false,
-            scores: {
-                density: {
-                    name: 'Density Done Well',
-                    score: 'A+',
-                    statement: 'Wooo housing choices for everyone, I really want people to have houses close to where they live and play. With good public transport access'
-                },
-                climateChange : {
-                    name: 'Climate Change Ideas',
-                    score: 'A+',
-                    statement: 'Save the planet!'
 
+            councilors : [
+                {
+                    email: 'info@generationzero.org.nz',
+                    website: 'http://www.generationzero.org/',
+                    name: 'Generation Zero',
+                    board: 'Waitamata',
+                    avatar: 'gzLogo.jpg',
+                    grade: 'A+',
+                    expanded: false,
+                    scores: [
+                        {
+                            key: 'density',
+                            name: 'Density Done Well',
+                            score: 'A+',
+                            statement: 'Wooo housing choices for everyone, I really want people to have houses close to where they live and play. With good public transport access',
+                            avatar: 'DensityDoneWell.png'
+                        },
+                        {
+                            key: 'climate',
+                            name: 'Climate Change Ideas',
+                            score: 'A+',
+                            statement: 'Save the planet!',
+                            avatar: 'climatechange.png'
+
+                        },
+                    ]
                 },
-            }
+                {
+                    email: 'admin@greaterauckland.org.nz',
+                    website: 'http://www.greaterauckland.org.nz/',
+                    name: 'Greater Auckland',
+                    board: 'Albert-Eden',
+                    avatar: 'greaterAucklandLogo.jpg',
+                    grade: 'A+',
+                    expanded: false,
+                    scores: [
+                        {
+                            key: 'density',
+                            name: 'Density Done Well',
+                            score: 'A+',
+                            statement: 'Wooo housing choices for everyone, I really want people to have houses close to where they live and play. With good public transport access',
+                            avatar: 'DensityDoneWell.png'
+                        },
+                        {
+                            key: 'climate',
+                            name: 'Climate Change Ideas',
+                            score: 'A+',
+                            statement: 'Save the planet!',
+                            avatar: 'climatechange.png'
+
+                        },
+                    ]
+                }
+            ]
+
         };
 
         this.handleWebsiteClick = this.handleWebsiteClick.bind(this);
@@ -41,21 +77,23 @@ export default class councillorCard extends React.Component {
         this.handleReduce = this.handleReduce.bind(this);
     }
 
-    handleWebsiteClick() {
-        window.open(this.state.website, '_blank');
+    handleWebsiteClick(website) {
+        window.open(website, '_blank');
     }
 
     handleExpandChange (expanded) {
         this.setState({expanded: expanded});
     };
 
-    handleExpand(){
-        this.setState({expanded: true});
+    handleExpand(expanded){
+        expanded = true;
     };
 
-    handleReduce () {
-        this.setState({expanded: false});
+    handleReduce (expanded) {
+        expanded = false;
     };
+
+
 
      render() {
          const grade = {
@@ -63,52 +101,57 @@ export default class councillorCard extends React.Component {
              fontSize: '300%'
          };
 
+         const councillors = this.state.councilors.map(councillor =>
+         <span id="councillorCard">
+             <Card
+                 key={councillor.name}
+                 expanded={councillor.expanded}>
+                 {console.debug(councillor.expanded)}
+                 <CardHeader
+                     title={councillor.name}
+                     subtitle={councillor.board + ' local board'}
+                     avatar={councillor.avatar}
+                     actAsExpander={true}
+                     showExpandableButton={true}
+                 >
+                     <span style={grade}>{councillor.grade}</span>
+                 </CardHeader>
+                 <CardText>
+                     <List>
+                         <ListItem
+                             primaryText={councillor.email}
+                             leftIcon={<Email />}
+                             disabled
+                         />
+                         <ListItem
+                             primaryText={councillor.website}
+                             leftIcon={<Link />}
+                             onClick={() => this.handleWebsiteClick(councillor.website)}
+                         />
+                     </List>
+                 </CardText>
+                 <CardText expandable={true}>
+                     <list>
+                         {councillor.scores.map(
+                             score => <ListItem
+                                                key={score.key}
+                                                primaryText={score.name}
+                                                leftAvatar={<Avatar size={40} src={score.avatar}/>}
+                                                rightAvatar={<Avatar>{score.score}</Avatar>}/>
+                            )
+                         }
+                     </list>
+                 </CardText>
+                 <CardActions>
+                     { !councillor.expanded ? <FlatButton label="Show scores" onTouchTap={() => this.handleExpand(councillor.expanded)} /> : <FlatButton label="Hide scores" onTouchTap={() => this.handleReduce(councillor.expanded)} /> }
+                 </CardActions>
+             </Card>
+         </span>
+         );
+
         return(
-            <div id="councillorCard">
-                <Card expanded={this.state.expanded} onExpandChange={this.handleExpandChange}>
-                    <CardHeader
-                        title={this.state.name}
-                        subtitle={this.state.board + ' local board'}
-                        avatar={this.state.avatar}
-                        actAsExpander={true}
-                        showExpandableButton={true}
-                    >
-                        <span style={grade}>{this.state.grade}</span>
-                    </CardHeader>
-                    <CardText>
-                        <List>
-                            <ListItem
-                                primaryText={this.state.email}
-                                leftIcon={<Email />}
-                                disabled
-                            />
-                            <ListItem
-                                primaryText={this.state.website}
-                                leftIcon={<Link />}
-                                onClick={this.handleWebsiteClick}
-                            />
-                        </List>
-                    </CardText>
-                    <CardText expandable={true}>
-                        <List>
-                            <ListItem
-                                primaryText={this.state.scores.density.name}
-                                leftAvatar={<Avatar size={40} src={'DensityDoneWell.png'}/>}
-                                rightAvatar={<Avatar>{this.state.scores.density.score}</Avatar>}
-                                secondaryText={this.state.scores.density.statement}
-                            />
-                            <ListItem
-                                primaryText={this.state.scores.climateChange.name}
-                                leftAvatar={<Avatar size={40} src={'climatechange.png'}/>}
-                                rightAvatar={<Avatar>{this.state.scores.climateChange.score}</Avatar>}
-                                secondaryText={this.state.scores.climateChange.statement}
-                            />
-                        </List>
-                    </CardText>
-                    <CardActions>
-                        { !this.state.expanded ? <FlatButton label="Show scores" onTouchTap={this.handleExpand} /> : <FlatButton label="Hide scores" onTouchTap={this.handleReduce} /> }
-                    </CardActions>
-                </Card>
+            <div id="container">
+                {councillors}
             </div>
         );
     }
